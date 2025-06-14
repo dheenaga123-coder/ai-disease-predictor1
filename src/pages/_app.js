57de74f4e40b5883e913e2a2dadffc60bd67ee35
dheenaga123-coder <dheenaga8@gmail.com>
2025-06-14 20:58:45 +0530

    Integrated Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import "@/styles/globals.css";
+// src/pages/_app.js
+
+import "@/styles/globals.css"; // keep your global styles
+import { Analytics } from '@vercel/analytics/react'; // add this line
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics /> {/* add the analytics component */}
+    </>
+  );
 }
